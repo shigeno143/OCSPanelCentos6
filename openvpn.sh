782310d4c6dc81586b36be1fb37d40c8725288e0
shigenojay <38946650+shigenojay@users.noreply.github.com>
2018-12-21 07:13:32 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -113,7 +113,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/shigeno143/OCSPanelCentos6/master/iptables.up.rules"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
