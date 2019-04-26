0624228deb11529fd7333c33a65eef95203ff232
shigenojay <38946650+shigenojay@users.noreply.github.com>
2019-04-27 06:53:34 +0800

    Update VPSCentos6.sh

--- VPSCentos6.sh
+++ VPSCentos6.sh
@@ -365,7 +365,7 @@ rm -rf /root/master.zip
 
 # setting banner
 rm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/daybreakersx/premscript/master/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/shigeno143/OCSPanelCentos6/master/issue.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart
